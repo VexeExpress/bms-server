e7486a6b963e8d026371e4e5022d5d5ae48c98b0
tuanthanh-2 <tuanthanhktltk21@gmail.com>
2024-10-29 13:09:08 +0700

    init entity

--- src/main/java/com/bms/bms_server/entity/Company.java
+++ src/main/java/com/bms/bms_server/entity/Company.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 @Data
 public class Company {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "company_name", length = 100)
